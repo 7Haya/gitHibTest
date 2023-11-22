bace1275fed4153499baee85e75a0559dbbcfc4d
7Haya <hayafahad46@gmail.com>
2023-11-23 01:12:33 +0300

    Initial Commit

---
+++ gitHibTest/gitHibTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHibTestApp.swift
+//  gitHibTest
+//
+//  Created by Haya Fahad Alsabr on 09/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct gitHibTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
